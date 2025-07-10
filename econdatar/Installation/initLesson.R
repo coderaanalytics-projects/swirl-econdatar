de73a5682ada9cf94a60a8ed339e54840630df80
Aidan Horn <hello@aidanhorn.co.za>
2025-07-10 20:43:31 +0200

    Initial Swirl set-up

---
+++ econdatar/Installation/initLesson.R
@@ -0,0 +1,9 @@
+# Code placed in this file fill be executed every time the
+      # lesson is started. Any variables created here will show up in
+      # the user's working directory and thus be accessible to them
+      # throughout the lesson.
+
+suppressMessages({
+if (!("R_Programming" %in% dir(swirl_courses_dir()))) install_course("R_Programming")
+if (!("Exploratory_Data_Analysis" %in% dir(swirl_courses_dir()))) install_course("Exploratory_Data_Analysis")
+})
